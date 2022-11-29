7eb7304e8c98504feadbbc1489b09bdc593cebc1
jtompkins415 <john.tompkins415@gmail.com>
2022-11-29 08:04:20 -0800

    remove uuid dependency, edit addTodo function, add id and key attribute to render

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,12 +1,11 @@
 import React, {useState} from 'react';
-import {v4 as uuid} from uuid;
 import Todo from './Todo';
 import NewTodoForm from './NewTodoForm';
 
 const TodoList = () => {
     const [todos, setTodos] = useState([])
     const addTodo = (newTodo) => {
-        setTodos(todos => [...todos, {newTodo, id: uuid()}])
+        setTodos(todos => [...todos, newTodo])
     }
 
     return(
@@ -14,8 +13,10 @@ const TodoList = () => {
             <h1>To Do List</h1>
             <NewTodoForm addTodo={addTodo} />
             <div>
-                {todos.map(({id, todo}) => <Todo todo={todo} /> )}
+                {todos.map(({id, todo}) => <Todo id={id} todo={todo} key={id}/> )}
             </div>
         </div>
     )
 }
+
+export default TodoList;
