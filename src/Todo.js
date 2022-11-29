ac18e5e2e0d3f25c68c571b8fda7e3d0ee01ff2f
jtompkins415 <john.tompkins415@gmail.com>
2022-11-29 08:10:38 -0800

    add remove button

--- src/Todo.js
+++ src/Todo.js
@@ -1,10 +1,12 @@
 import React from "react";
 
-const Todo = ({id, todo='default todo'}) => {
+const Todo = ({id, todo='default todo', remove}) => {
+     const handleDelete = () => remove(id)
     return (
         <div>
             <ul>
             <li>{todo}</li>
+            <button onClick={handleDelete}>X</button>
             </ul>
         </div>
     )
